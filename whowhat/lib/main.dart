08a285cd2e8f77243830dfdb17f3fc4d09ea0527
Muriel Pinho <muriel.apinho@gmail.com>
2020-11-05 19:30:44 +0000

    Changed statusbar text color to white in iOS

--- whowhat/lib/main.dart
+++ whowhat/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:whowhat/templates/menu.dart';
+import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
 //import 'package:whowhat/templates/connection.dart';
 
 void main() {
@@ -10,6 +11,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    FlutterStatusbarcolor.setStatusBarWhiteForeground(true);
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
