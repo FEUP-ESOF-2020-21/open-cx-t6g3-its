771ba23314013f656f03677f368bb107547f8caa
Rodrigo Reis <rodrigocreis.12@gmail.com>
2020-12-11 12:08:12 +0000

    🚧 Adding user to session

--- whowhat/lib/widgets/database/session_connection.dart
+++ whowhat/lib/widgets/database/session_connection.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -24,7 +25,7 @@ class UserSessionInformation extends StatelessWidget {
           return Text("Loading");
         }
 
-        int nUsers = snapshot.data.docs.length - 1;
+        int nUsers = snapshot.data.docs.length;
         if (!(nUsers >= 0)) {
           nUsers = 0;
         }
@@ -66,11 +67,12 @@ Future<bool> availableSession(String session) async {
 }
 
 void joinSession(String session) async {
+  FirebaseAuth auth = FirebaseAuth.instance;
   // Get reference to Firestore collection
   var databaseReference = FirebaseFirestore.instance
       .collection('sessions')
       .doc(session)
       .collection('users');
 
-  await databaseReference.add({});
+  await databaseReference.doc(auth.currentUser.uid).set({});
 }
